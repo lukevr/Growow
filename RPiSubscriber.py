d83820df8ca7b21aa824f06296be101330419415
Victor Radchenko <lukevr@gmail.com>
2016-03-19 01:28:56 +0200

    draft

--- RPiSubscriber.py
+++ RPiSubscriber.py
@@ -1,6 +1,11 @@
 __author__ = 'luke'
 
 import zmq
+import os
+import time
+import signal
+import subprocess
+
 # import sys
 
 
@@ -15,16 +20,37 @@ def main():
     context = zmq.Context()
     subscriber = context.socket(zmq.SUB)
     subscriber.connect(string)
-    subscriber.setsockopt(zmq.SUBSCRIBE, b"B")
+    subscriber.setsockopt(zmq.SUBSCRIBE, b"P")
+    subscriber.setsockopt(zmq.SUBSCRIBE, b"S")
 
+    pro = None
     while True:
         # Read envelope with address
         [address, contents] = subscriber.recv_multipart()
-        print("[%s] %s" % (address, contents))
+
+        if address == "P" and not is_process_running(pro):
+            # Play
+            cmd = "raspivid -vf -n -w 640 -h 480 -t 0 -b 2000000 | nc 178.214.221.154 5777"
+            pro = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+            print("[%s] %s -- %s" % (address, contents, pro))
+        if address == "S" and pro is not(None) and is_process_running(pro):
+            os.killpg(os.getpgid(pro.pid), signal.SIGTERM)
+            print("[%s] %s -- %s" % (address, contents, pro))
+        address == 0;
+        time.sleep(0.5)
 
     # We never get here but clean up anyhow
     subscriber.close()
     context.term()
+
+def is_process_running(pro):
+    if pro is None:
+        return False
+    try:
+        os.kill(pro.pid, 0)
+        return True
+    except OSError:
+        return False
 
 if __name__ == "__main__":
     main()
