361e8c6fab81c57c2065225a1b4938be94925fed
Victor Radchenko <lukevr@gmail.com>
2016-03-18 23:45:04 +0200

    Subscriber

---
+++ RPiSubscriber.py
@@ -0,0 +1,30 @@
+__author__ = 'luke'
+
+import zmq
+# import sys
+
+
+def main():
+    #ip = sys.argv[1]
+    ip = "178.214.221.154"
+
+    port = 5563
+    string = "tcp://" + ip + ":" + str(port)
+
+    # Prepare our context and publisher
+    context = zmq.Context()
+    subscriber = context.socket(zmq.SUB)
+    subscriber.connect(string)
+    subscriber.setsockopt(zmq.SUBSCRIBE, b"B")
+
+    while True:
+        # Read envelope with address
+        [address, contents] = subscriber.recv_multipart()
+        print("[%s] %s" % (address, contents))
+
+    # We never get here but clean up anyhow
+    subscriber.close()
+    context.term()
+
+if __name__ == "__main__":
+    main()
